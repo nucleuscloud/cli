e36d5cc7fd214e05212d764f62f5662fba3fe5c3
Nick Z <2420177+nickzelei@users.noreply.github.com>
2022-07-27 10:17:14 -0700

    fix build errors

--- cmd/accounts_invite.go
+++ cmd/accounts_invite.go
@@ -55,11 +55,7 @@ func init() {
 }
 
 func inviteUser(email string) error {
-	conn, err := utils.NewApiConnection(utils.ApiConnectionConfig{
-		AuthBaseUrl:  utils.Auth0BaseUrl,
-		AuthClientId: utils.Auth0ClientId,
-		ApiAudience:  utils.ApiAudience,
-	})
+	conn, err := utils.NewApiConnectionByEnv(utils.GetEnv())
 	if err != nil {
 		return err
 	}
--- cmd/accounts_rm.go
+++ cmd/accounts_rm.go
@@ -54,11 +54,7 @@ func init() {
 }
 
 func removeUser(email string) error {
-	conn, err := utils.NewApiConnection(utils.ApiConnectionConfig{
-		AuthBaseUrl:  utils.Auth0BaseUrl,
-		AuthClientId: utils.Auth0ClientId,
-		ApiAudience:  utils.ApiAudience,
-	})
+	conn, err := utils.NewApiConnectionByEnv(utils.GetEnv())
 	if err != nil {
 		return err
 	}
