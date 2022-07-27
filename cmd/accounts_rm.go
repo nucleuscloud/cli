1df82a00995af57b83e6a09fa51d432333e4d93f
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2022-07-27 10:18:56 -0700

    Nick/nuc 22 account mgmt (#62)
    * adds command to send out account invites
    
    * acc mgmt in cli
    
    * updating go mod
    
    * remove unneeded println
    
    * fix build errors

---
+++ cmd/accounts_rm.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"net/mail"
+
+	"github.com/nucleuscloud/api/pkg/api/v1/pb"
+	"github.com/nucleuscloud/cli/internal/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var accountsRmCmd = &cobra.Command{
+	Use: "rm <email>",
+	Aliases: []string{
+		"remove",
+	},
+	Short: "Allows you to remove a user to your account",
+	Long:  "Removes a user from accessing your account. *Note: You can't remove yourself or the account owner",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("must provide an email to remove someone from your account")
+		}
+		if len(args) > 1 {
+			return fmt.Errorf("may only remove one email at a time")
+		}
+		email := args[0]
+		_, err := mail.ParseAddress(email)
+		if err != nil {
+			return err
+		}
+
+		return removeUser(email)
+	},
+}
+
+func init() {
+	accountsCmd.AddCommand(accountsRmCmd)
+}
+
+func removeUser(email string) error {
+	conn, err := utils.NewApiConnectionByEnv(utils.GetEnv())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	fmt.Println("Removing user...")
+	cliClient := pb.NewCliServiceClient(conn)
+	_, err = cliClient.RemoveUserFromAccount(context.Background(), &pb.RemoveUserFromAccountRequest{
+		Email: email,
+	})
+	return err
+}
--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -52,6 +53,7 @@ require (
 	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220719170305-83ca9fad585f // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
