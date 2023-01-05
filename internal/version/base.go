d69a7a13051d503fb0fd58670b6c35e3420c5e91
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-01-05 12:35:32 -0800

    Adds iso format comment

--- internal/version/base.go
+++ internal/version/base.go
@@ -4,5 +4,5 @@ var (
 	// These variables are replaced by ldflags at build time
 	gitVersion = "v0.0.0-main"
 	gitCommit  = ""
-	buildDate  = "1970-01-01T00:00:00Z"
+	buildDate  = "1970-01-01T00:00:00Z" // build date in ISO8601 format
 )
